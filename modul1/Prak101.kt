f53e66a2947b764976715a7908324279e0a7f540
Ferry Roland <101640525+ferryszhtan81@users.noreply.github.com>
2022-03-20 09:24:08 +0800

    Create Prak101.kt

---
+++ modul1/Prak101.kt
@@ -0,0 +1,16 @@
+import java.util.Scanner
+fun main(){
+    val masukkansuhu= Scanner(System.`in`)
+    println("Waktu Sekarang:")
+    val waktu= readLine()!!
+    println("Nama Anda:")
+    val nama= readLine()!!
+    println("Umur Anda:")
+    val umur= Integer.valueOf(readLine())
+    println("Suhu Tubuh Anda:")
+    val suhutubuh= masukkansuhu.nextFloat()
+
+    println("Selamat $waktu, $nama.")
+    println("Umur anda $umur Tahun.")
+    println("Suhu Tubuh anda $suhutubuh derajat Celcius.")
+}
