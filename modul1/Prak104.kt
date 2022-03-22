086d343ba650e8c5867030d87137fa2db2d2add4
Ferry Roland <101640525+ferryszhtan81@users.noreply.github.com>
2022-03-22 22:14:15 +0800

    Update Prak104.kt

--- modul1/Prak104.kt
+++ modul1/Prak104.kt
@@ -8,6 +8,8 @@ fun main() {
     val kucing = kucing("Ruak", "Kucing", 2, "Jantan")
     val tupai = tupai("Hala", "Tupai", 4, "Betina")
 
-    println("Hai, saya mempunyai ${kucing.Jenis} yang bernama ${kucing.Nama}. Dia berumur ${kucing.Umur} tahun dan dia adalah kucing ${kucing.Kelamin}.")
-    println("Selain itu, saya juga memliki ${tupai.Jenis} yang bernama ${tupai.Nama}. Dia lebih tua daripada ${kucing.Jenis}ku, dia itu berumur ${tupai.Umur} dan dia itu adalah tupai ${tupai.Kelamin}.")
+    println("Hai, saya mempunyai ${kucing.Jenis} yang bernama ${kucing.Nama}. " +
+            "Dia berumur ${kucing.Umur} tahun dan dia adalah kucing ${kucing.Kelamin}.")
+    println("Selain itu, saya juga memliki ${tupai.Jenis} yang bernama ${tupai.Nama}. " +
+            "Dia lebih tua daripada ${kucing.Jenis}ku, dia itu berumur ${tupai.Umur} dan dia itu adalah tupai ${tupai.Kelamin}.")
 }
