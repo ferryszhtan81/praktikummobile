37c016884eb5cc4510e9c35a6961914179d92c5f
Ferry Roland <101640525+ferryszhtan81@users.noreply.github.com>
2022-03-23 23:14:46 +0800

    Update Prak103.kt

--- modul1/Prak103.kt
+++ modul1/Prak103.kt
@@ -1,1 +1,12 @@
-
+internal object PRAK103 {
+    private fun HASIL(N: Int) {
+        for (num in 0 until N * N) {
+            if (num % 2 == 0 && num % 3 == 0) print("$num ")
+        }
+    }
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val N = Integer.valueOf(readLine())
+        HASIL(N)
+    }
+}
