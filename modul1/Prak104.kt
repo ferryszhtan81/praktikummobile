12a3b0bcb7bce91785cbadd300218764738c6d75
Ferry Roland <101640525+ferryszhtan81@users.noreply.github.com>
2022-03-23 22:19:55 +0800

    Update Prak104.kt

--- modul1/Prak104.kt
+++ modul1/Prak104.kt
@@ -1,15 +1,22 @@
 class kucing(var Nama: String, var Jenis: String, var Umur: Int, var Kelamin: String){
-
+    fun SukaKucing(){ println("Saya suka banget dengan kucing") }
 }
 class tupai(var Nama: String,var Jenis: String, var Umur: Int, var Kelamin: String){
-
+    fun SukaTupai(){ println("Dan tupai yang kumiliki! Mereka itu hewan menggemaskan.")}
 }
 fun main() {
     val kucing = kucing("Ruak", "Kucing", 2, "Jantan")
     val tupai = tupai("Hala", "Tupai", 4, "Betina")
-
-    println("Hai, saya mempunyai ${kucing.Jenis} yang bernama ${kucing.Nama}. " +
-            "Dia berumur ${kucing.Umur} tahun dan dia adalah kucing ${kucing.Kelamin}.")
-    println("Selain itu, saya juga memliki ${tupai.Jenis} yang bernama ${tupai.Nama}. " +
-            "Dia lebih tua daripada ${kucing.Jenis}ku, dia itu berumur ${tupai.Umur} dan dia itu adalah tupai ${tupai.Kelamin}.")
+    var hallo = object {
+        val Hai = "Hai semuanya, "
+        val Roland = "ini Ferry Roland"
+        override fun toString(): String = "$Hai$Roland"
+    }
+    println("$hallo, saya ingin mau kasih tau nih")
+    println("Kalau saya punya ${kucing.Jenis} yang bernama ${kucing.Nama}. ")
+    println("Dia berumur ${kucing.Umur} tahun dan dia adalah kucing ${kucing.Kelamin}.")
+    println("Selain itu, saya juga memliki ${tupai.Jenis} yang bernama ${tupai.Nama}.")
+    println("Dia lebih tua daripada ${kucing.Jenis}ku, dia itu berumur ${tupai.Umur}, ")
+    println("Dan dia itu adalah tupai ${tupai.Kelamin} yang hebat.")
+    kucing.SukaKucing(); tupai.SukaTupai()
 }
